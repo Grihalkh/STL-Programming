a12888f088ff20b6462317d860f84b06e90ddb24
Grigory Khromov <38518245+Grihalkh@users.noreply.github.com>
2020-02-01 15:23:31 +0300

    task 3 done

--- task3.h
+++ task3.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #include <set>
+#include <vector>
 #include <time.h>
 #include <iostream>
 #include <algorithm>
-#include <iterator>
 
 // Enter U from console;
 // A is picked at random (random indices from U);
@@ -17,18 +17,27 @@
 // Output 30% largest elements
 
 template<typename T>
+void print_vector(const std::vector<T>& s)
+{
+	std::for_each(s.begin(), s.end(), [](const T& i)
+		{
+			std::cout << i << " ";
+		});
+
+	std::cout << std::endl;
+}
+
+template<typename T>
 void print_set(const std::set<T> &s)
 {
-	for (auto i = s.begin(); i != s.end(); ++i)
-		std::cout << *i << " ";
-	/*
 	std::for_each(s.begin(), s.end(), [](const T &i) 
 		{
 			std::cout << i << " ";
 		});
-	*/
+	
 	std::cout << std::endl;
 }
+
 
 void task()
 {
@@ -58,6 +67,7 @@ void task()
 	}
 
 	std::cout << "Set A:" << std::endl;
+
 	print_set(A);
 
 	// Enter B
@@ -74,17 +84,62 @@ void task()
 			B.insert(tmp);
 		}
 
-		std::set<int> intersection;
-		std::set_intersection(A.begin(), A.end(), B.begin(), B.end(), intersection);
+		std::vector<int> intersection;
+		std::set_intersection(A.begin(), A.end(), B.begin(), B.end(), std::back_inserter(intersection));
 		// Checks
 		if (std::includes(U.begin(), U.end(), B.begin(), B.end()) && // is in U
-			!std::equal(A.begin(), A.end(), B.begin()) && // A != B
-			intersection.size()) // A and B have an intersection
+			intersection.size() && // A and B have an intersection
+			!(intersection.size() == A.size() && intersection.size() == B.size()) // A != B
+			)
+		{
 			bSetOk = true;
+		}
 		else
 		{
 			B.empty();
 			std::cout << "You have entered B incorrectly! Repeat input." << std::endl;
 		}
 	}
+
+	// Main Prog
+	// A and B
+	std::vector<int> intersection1;
+	std::set_intersection(A.begin(), A.end(), B.begin(), B.end(), std::back_inserter(intersection1));
+
+	// not A, not B
+	std::vector<int> notA, notB;
+	std::set_difference(U.begin(), U.end(), A.begin(), A.end(), std::back_inserter(notA));
+	std::set_difference(U.begin(), U.end(), B.begin(), B.end(), std::back_inserter(notB));
+
+	// not A or not B
+	std::vector<int> tmp;
+	std::set_union(notA.begin(), notA.end(), notB.begin(), notB.end(), std::back_inserter(tmp));
+	// not (not A or not B)
+	std::vector<int> intersection2;
+	std::set_difference(U.begin(), U.end(), tmp.begin(), tmp.end(), std::back_inserter(intersection2));
+
+	// Output
+	std::cout << "A and B:" << std::endl;
+	print_vector(intersection1);
+	std::cout << "not (not A and not B):" << std::endl;
+	print_vector(intersection2);
+
+	// out 30 % largest
+	std::cout << "30% largest:" << std::endl;
+	if (std::equal(intersection1.begin(), intersection1.end(), intersection2.begin()))
+	{
+		int n = 0.3 * intersection1.size();
+		if (n == 0)
+		{
+			std::cout << "There 30% gives us 0 elements :)" << std::endl;
+			return;
+		}
+		for (int i = 0; i < n; ++i)
+		{
+			auto max = std::max_element(intersection1.begin(), intersection1.end());
+			std::cout << *max << " ";
+			intersection1.erase(max);
+		}
+	}
+	std::cout << std::endl;
 }
